8893b545023c8916c7fe197672ad0fd9f7771262
Henrique de Lima <167571426+HenriqueDeLima8@users.noreply.github.com>
2025-05-27 22:05:12 -0300

    arrowFunction.js
    apenas no console do navegador

--- arrowFunction.js
+++ arrowFunction.js
@@ -1,21 +1,13 @@
-const readline = require('readline');
-
-const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
-});
-
 const _velocidadeMedia = (distancia, tempo) => {
+
     let velocidadeMedia = distancia / tempo;
-    console.log("Arrow Function ---->");
-    console.log("A Velocidade Média foi de " + velocidadeMedia + " metros por minuto");
+
+console.log("Arrow Function ---->");
+console.log("A Velocidade Média foi de " + velocidadeMedia + " metros por minuto");
+
 };
 
-rl.question("Digite a distância (metros): ", (distancia) => {
-    rl.question("Digite o tempo (minutos): ", (tempo) => {
-        let numero1 = parseFloat(distancia);
-        let numero2 = parseFloat(tempo);
-        _velocidadeMedia(numero1, numero2);
-        rl.close();
-    });
-});
+let numero1 = parseFloat(prompt("Digite a distancia (metros)"));
+let numero2 = parseFloat(prompt("Digite o tempo (minutos)"));
+
+_velocidadeMedia(numero1, numero2);
